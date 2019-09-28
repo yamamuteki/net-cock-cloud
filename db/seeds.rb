dc5cc935c64b133b7462cb5b7410e41ac4c19237
yama@muteki <yama@muteki.com>
2019-09-29 01:47:45 +0900

    Seed board and author

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Board.delete_all
+Author.delete_all
 Article.delete_all
 
 files = ['db/POPO_03.nck', 'db/POPO_03.LOG', 'db/POPO_05.nck', 'db/POPO_05.LOG']
 
 files.each do |file|
-    File.open(file, mode = "rt:windows-31j:utf-8", invalid: :replace, undef: :replace) do |f|
+    File.open(file, mode = "rt:cp932:utf-8", invalid: :replace, undef: :replace) do |f|
         input = f.read
         articles_text = input.split("[32m-	-	-	-	-	-	-	-	-[m\n")
-        puts board_title = articles_text.shift
+        puts board_title = articles_text.shift.strip
+        board = Board.find_or_create_by(title: board_title)
         articles_text.each do |article_text|
             article_texts = article_text.split("\n")
             puts article_header = article_texts[0]
-            puts article_desc1 = article_texts[1]
-            puts article_desc2 = article_texts[2]
+            puts article_desc1 = article_texts[1].rstrip
+            puts article_desc2 = article_texts[2].rstrip
             puts board_name = article_header.split[0].split('(')[0]
-            # puts id = article_header.split[0].match(/\d+/)[0]
             puts title = article_header.split(" ", 4)[3]
             puts posted_at = DateTime.parse(article_header.split[1] + " " + article_header.split[2] + "+09:00")
-            Article.create(title: title, body: article_text, posted_at: posted_at)
+            puts author_name = if article_desc1.start_with?("  ")
+                article_desc1.match(/:(.*)/)[1]
+            else
+                article_desc2.match(/:(.*)/)[1]
+            end
+            author = Author.find_or_create_by(name: author_name)
+            Article.create(title: title, body: article_text, posted_at: posted_at, author: author, board: board)
         end
     end
 end
