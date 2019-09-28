2ae56a3cb8c4337261eef658a079f094012944f5
yama@muteki <yama@muteki.com>
2019-09-29 03:02:52 +0900

    Add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'boards#index'
   resources 'articles', only: [:index, :show]
   resources 'boards', only: [:index, :show]
   resources 'authors', only: [:index, :show]
