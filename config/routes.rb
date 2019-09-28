eea00f3573e3f0880461e1ff52da4e9732427e70
yama@muteki <yama@muteki.com>
2019-09-29 02:35:13 +0900

    Add authors controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources 'articles', only: [:index, :show]
   resources 'boards', only: [:index, :show]
+  resources 'authors', only: [:index, :show]
 end
