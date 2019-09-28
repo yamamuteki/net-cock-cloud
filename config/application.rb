74bcd0ba3c680c4d86c26ff1c3ee126082f79aae
yama@muteki <yama@muteki.com>
2019-09-29 06:58:04 +0900

    Add ja.yml

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,7 @@ module NetCockCloud
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+    config.i18n.available_locales = [:en, :ja]
+    config.i18n.default_locale = :ja
   end
 end
